2b2f3e39fb66073a3e3a25cc098fb46982eef957
Rober Maher <145109228+RoberMaher@users.noreply.github.com>
2024-02-25 21:56:01 +0200

    GPIO Driver

--- ARM_COTS/MCAL/GPIO/GPIO_Program.c
+++ ARM_COTS/MCAL/GPIO/GPIO_Program.c
@@ -27,24 +27,280 @@
 
 
 //Pre_Build Configuration
-void GPIO_voidSetPinMode(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_MODE_e PinMode)
+void GPIO_voidSetPinMode(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_MODE_e pinMode)
 {
-	
+	if((portID <= GPIO_PORTC) && (pinID <= PIN15))
+	{
+		switch(portID)
+		{
+			case GPIO_PORTA:
+				switch(pinMode)
+				{
+					case GPIO_INPUT:
+						CLR_BIT(GPIOA_MODER,(pinID*2);
+						CLR_BIT(GPIOA_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT:
+						CLR_BIT(GPIOA_MODER,(pinID*2);
+						SET_BIT(GPIOA_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ALTERNATE_FUNCTION:
+						SET_BIT(GPIOA_MODER,(pinID*2);
+						CLR_BIT(GPIOA_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ANALOG:
+						SET_BIT(GPIOA_MODER,(pinID*2);
+						SET_BIT(GPIOA_MODER,((pinID*2)+1));
+					break;
+				}
+			break;
+			
+			case GPIO_PORTB:
+				switch(pinMode)
+				{
+					case GPIO_INPUT:
+						CLR_BIT(GPIOB_MODER,(pinID*2);
+						CLR_BIT(GPIOB_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT:
+						CLR_BIT(GPIOB_MODER,(pinID*2);
+						SET_BIT(GPIOB_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ALTERNATE_FUNCTION:
+						SET_BIT(GPIOB_MODER,(pinID*2);
+						CLR_BIT(GPIOB_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ANALOG:
+						SET_BIT(GPIOB_MODER,(pinID*2);
+						SET_BIT(GPIOB_MODER,((pinID*2)+1));
+					break;
+				}
+			break;
+			
+			case GPIO_PORTC:
+				switch(pinMode)
+				{
+					case GPIO_INPUT:
+						CLR_BIT(GPIOC_MODER,(pinID*2);
+						CLR_BIT(GPIOC_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT:
+						CLR_BIT(GPIOC_MODER,(pinID*2);
+						SET_BIT(GPIOC_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ALTERNATE_FUNCTION:
+						SET_BIT(GPIOC_MODER,(pinID*2);
+						CLR_BIT(GPIOC_MODER,((pinID*2)+1));
+					break;
+					
+					case GPIO_ANALOG:
+						SET_BIT(GPIOC_MODER,(pinID*2);
+						SET_BIT(GPIOC_MODER,((pinID*2)+1));
+					break;
+				}
+			break;
+		}
+	}
+	else
+	{
+		// Do Nothing
+	}
 }
 
-void GPIO_voidSetPinOutputMode(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_OUTPUT_MODE_e PinOutputMode)
+void GPIO_voidSetPinOutputMode(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_OUTPUT_MODE_e pinOutputMode)
 {
-	
+	if((portID <= GPIO_PORTC) && (pinID <= PIN15) && (pinOutputMode <= GPIO_OUTPUT_OPEN_DRAIN))
+	{
+		switch(portID)
+		{
+			case GPIO_PORTA:
+				switch(pinOutputMode)
+				{
+					case GPIO_OUTPUT_PUSH_PULL:
+						CLR_BIT(GPIOA_OTYPER, pinID);
+					break;
+					
+					case GPIO_OUTPUT_OPEN_DRAIN:
+						SET_BIT(GPIOA_OTYPER, pinID);
+					break;
+				}
+			break;
+		
+			case GPIO_PORTB:
+				switch(pinOutputMode)
+				{
+					case GPIO_OUTPUT_PUSH_PULL:
+						CLR_BIT(GPIOB_OTYPER, pinID);
+					break;
+					
+					case GPIO_OUTPUT_OPEN_DRAIN:
+						SET_BIT(GPIOB_OTYPER, pinID);
+					break;
+				}
+			break;
+
+			case GPIO_PORTC:
+				switch(pinOutputMode)
+				{
+					case GPIO_OUTPUT_PUSH_PULL:
+						CLR_BIT(GPIOC_OTYPER, pinID);
+					break;
+					
+					case GPIO_OUTPUT_OPEN_DRAIN:
+						SET_BIT(GPIOC_OTYPER, pinID);
+					break;
+				}
+			break;
+			
+		}
+	}
+	else
+	{
+		// Do Nothing
+	}
 }
 
-void GPIO_voidSetPinOutputSpeed(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_OUTPUT_SPEED_e PinOutputSpeed)
+void GPIO_voidSetPinOutputSpeed(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_OUTPUT_SPEED_e pinOutputSpeed)
 {
-	
+	if((portID <= GPIO_PORTC) && (pinID <= PIN15))
+	{
+		switch(portID)
+		{
+			case GPIO_PORTA:
+				switch(pinOutputSpeed)
+				{
+					case GPIO_OUTPUT_LOW:
+						CLR_BIT(GPIOA_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOA_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_MEDIUM:
+						CLR_BIT(GPIOA_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOA_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_HIGH:
+						SET_BIT(GPIOA_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOA_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_VERY_HIGH:
+						SET_BIT(GPIOA_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOA_OSPEEDR,((pinID*2)+1));
+					break;
+				}
+			break;
+			
+			case GPIO_PORTB:
+				switch(pinOutputSpeed)
+				{
+					case GPIO_OUTPUT_LOW:
+						CLR_BIT(GPIOB_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOB_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_MEDIUM:
+						CLR_BIT(GPIOB_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOB_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_HIGH:
+						SET_BIT(GPIOB_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOB_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_VERY_HIGH:
+						SET_BIT(GPIOB_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOB_OSPEEDR,((pinID*2)+1));
+					break;
+				}
+			break;
+			
+			case GPIO_PORTC:
+				switch(pinOutputSpeed)
+				{
+					case GPIO_OUTPUT_LOW:
+						CLR_BIT(GPIOC_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOC_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_MEDIUM:
+						CLR_BIT(GPIOC_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOC_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_HIGH:
+						SET_BIT(GPIOC_OSPEEDR,(pinID*2);
+						CLR_BIT(GPIOC_OSPEEDR,((pinID*2)+1));
+					break;
+					
+					case GPIO_OUTPUT_VERY_HIGH:
+						SET_BIT(GPIOC_OSPEEDR,(pinID*2);
+						SET_BIT(GPIOC_OSPEEDR,((pinID*2)+1));
+					break;
+				}
+			break;
+		}
+	}
+	else
+	{
+		// Do Nothing
+	}
 }
 
-void GPIO_voidSetPinValue(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_VALUE_e PinValue)
+void GPIO_voidSetPinValue(GPIO_PORT_e portID, GPIO_PIN_e pinID, GPIO_VALUE_e pinValue)
 {
+	if((portID <= GPIO_PORTC) && (pinID <= PIN15) && (pinValue <= DIO_SET))
+	{
+		switch(portID)
+		{
+			case GPIO_PORTA:
+				switch(pinValue)
+				{
+					case GPIO_RESET:
+						SET_BIT(GPIOA_BSRR,(pinID+16));
+					case GPIO_SET:
+						SET_BIT(GPIOA_BSRR,pinID);
+					break;
+				}
+			break;
+			
+			case GPIO_PORTB:
+				switch(pinValue)
+				{
+					case GPIO_RESET:
+						SET_BIT(GPIOB_BSRR,(pinID+16));
+					case GPIO_SET:
+						SET_BIT(GPIOB_BSRR,pinID);
+					break;
+				}
+			break;
+			
+			case GPIO_PORTC:
+				switch(pinValue)
+				{
+					case GPIO_RESET:
+						SET_BIT(GPIOC_BSRR,(pinID+16));
+					case GPIO_SET:
+						SET_BIT(GPIOC_BSRR,pinID);
+					break;
+				}
+			break;
+		}
+	}
 	
+	else
+	{
+		// Do Nothing
+	}
 }
 
 u8 GPIO_u8GetPinValue(GPIO_PORT_e portID, GPIO_PIN_e pinID)
@@ -57,24 +313,64 @@ void GPIO_voidLockPin(GPIO_PORT_e portID, GPIO_PIN_e pinID)
 	
 }
 
-void GPIO_voidSetPortMode(GPIO_PORT_e portID, GPIO_MODE_e PortMode)
+void GPIO_voidSetPortMode(GPIO_PORT_e portID, GPIO_MODE_e portMode)
+ {
+	if ( (portID <= GPIO_PORTC) )
+	{
+		u32 modeValue = 0;
+		for (int i = 0; i < 16; i++) {
+			modeValue |= (portMode << (2 * i));
+		}
+	
+		switch (portID) {
+			case GPIO_PORTA:
+				GPIOA_MODER = modeValue;
+				break;
+			case GPIO_PORTB:
+				GPIOB_MODER = modeValue;
+				break;
+			case GPIO_PORTC:
+				GPIOC_MODER = modeValue;
+				break;
+		}
+	}
+	else
+	{
+		// Do Nothing
+	}
+}
+
+void GPIO_voidSetPortOutputMode(GPIO_PORT_e portID, GPIO_OUTPUT_MODE_e portOutputMode)
 {
 	
 }
 
-void GPIO_voidSetPortOutputMode(GPIO_PORT_e portID, GPIO_OUTPUT_MODE_e PortOutputMode)
+void GPIO_voidSetPortOutputSpeed(GPIO_PORT_e portID, GPIO_OUTPUT_SPEED_e portOutputSpeed)
 {
 	
 }
 
-void GPIO_voidSetPortOutputSpeed(GPIO_PORT_e portID, GPIO_OUTPUT_SPEED_e PortOutputSpeed)
+void GPIO_voidSetPortValue(GPIO_PORT_e portID, GPIO_VALUE_e portValue)
 {
-	
-}
-
-void GPIO_voidSetPortValue(GPIO_PORT_e portID, GPIO_VALUE_e PortValue)
-{
-	
+	if ( (portID <= GPIO_PORTC) )
+	{
+		switch (portID)
+		{
+		case GPIO_PORTA:
+			GPIOA_ODR = portValue;
+			break;
+		case GPIO_PORTB:
+			GPIOB_ODR = portValue;
+			break;
+		case GPIO_PORTC:
+			GPIOBCODR = portValue;
+			break;
+		}
+	}
+	else
+	{
+		// Do Nothing
+	}
 }
 
 u8 GPIO_u8GetPortValue(GPIO_PORT_e portID)
