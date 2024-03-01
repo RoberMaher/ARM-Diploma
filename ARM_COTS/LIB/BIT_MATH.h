1817fdb6c752e84a7f9cbbac5d18170edb583358
Rober Maher <145109228+RoberMaher@users.noreply.github.com>
2024-03-01 12:41:13 +0200

    LIB layer

--- ARM_COTS/LIB/BIT_MATH.h
+++ ARM_COTS/LIB/BIT_MATH.h
@@ -1,3 +1,16 @@
+/*********************************************
+ * Author:				Rober Maher
+ * Creation Data:		23 Feb, 2024
+ * Version:				v1.0
+ * Compiler:			GNU ARM-GCC
+ * Controller:			STM32F401CCU6
+ * Layer:				LIB
+ ********************************************/
+/*********************************************
+ * Version	  Date				  Author				  Description
+ * v1.0		  23 Feb, 2024		Rober Maher			    Initial Creation
+*********************************************/
+
 //Header Guard File
 #ifndef BIT_MATH_H
 #define BIT_MATH_H
--- ARM_COTS/LIB/STD_TYPES.h
+++ ARM_COTS/LIB/STD_TYPES.h
@@ -1,17 +1,31 @@
+/*********************************************
+ * Author:				Rober Maher
+ * Creation Data:		23 Feb, 2024
+ * Version:				v1.0
+ * Compiler:			GNU ARM-GCC
+ * Controller:			STM32F401CCU6
+ * Layer:				LIB
+ ********************************************/
+/*********************************************
+ * Version	  Date				  Author				  Description
+ * v1.0		  23 Feb, 2024		Rober Maher			    Initial Creation
+ * v2.0		  1  Mar, 2024		Rober Maher			    Solve Problem
+*********************************************/
+
 //Header Guard File
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
 
 //typedef new_datatype old_datatype
-typedef	u8	unsigned char;
-typedef	u16	unsigned short int;
-typedef	u32	unsigned int;
+typedef	unsigned char u8;
+typedef	unsigned short int u16;
+typedef	unsigned int u32;
 
-typedef	s8	signed char;
-typedef	s16	signed short;
-typedef	s32	signed int;
+typedef	signed char s8;
+typedef	signed short s16;
+typedef	signed int s32;
 
-typedef	f32	float;
-typedef	f64	double;
+typedef	float f32;
+typedef	double f64;
 
 #endif
