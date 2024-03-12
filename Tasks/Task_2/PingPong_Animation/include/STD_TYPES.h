b570e108f5b7a4041b65b3cbf75d43b13d8618cc
robermaher <robermaher@gmail.com>
2024-03-12 17:59:50 +0200

    Add Ping Pong Animation Task

---
+++ Tasks/Task_2/PingPong_Animation/include/STD_TYPES.h
@@ -0,0 +1,17 @@
+//Header Guard File
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+//typedef new_datatype old_datatype
+typedef	unsigned char u8;
+typedef	unsigned short int u16;
+typedef	unsigned int u32;
+
+typedef	signed char s8;
+typedef	signed short s16;
+typedef	signed int s32;
+
+typedef	float f32;
+typedef	double f64;
+
+#endif
