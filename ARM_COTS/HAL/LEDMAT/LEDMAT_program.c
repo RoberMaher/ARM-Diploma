ec5474d0cd2450fa34906bda245f94a7e38143b1
robermaher <robermaher@gmail.com>
2024-04-19 10:18:42 +0200

    Add LEDMatrix Driver

--- ARM_COTS/HAL/LEDMAT/LEDMAT_program.c
+++ ARM_COTS/HAL/LEDMAT/LEDMAT_program.c
@@ -26,65 +26,65 @@
 void HLEDMAT_voidInit(void)
 {
     /*Set Columns Pins To Be Output*/
-    MGPIO_voidSetPinMode(HLEDMAT_COL0_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL1_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL2_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL3_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL4_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL5_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL6_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_COL7_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL0_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL1_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL2_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL3_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL4_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL5_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL6_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_COL7_PORT_PIN, GPIO_OUTPUT);
 
     /*Set Columns Pins To Be Output PP*/
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL0_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL1_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL2_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL3_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL4_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL5_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL6_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_COL7_PORT_PIN, GPIO_OUTPUT_PP);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL0_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL1_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL2_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL3_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL4_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL5_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL6_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_COL7_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
 
     /*Set Columns Pin To Be Output Low Speed*/
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL0_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL1_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL2_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL3_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL4_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL5_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL6_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_COL7_PORT_PIN, GPIO_LOW_SPEED);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL0_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL1_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL2_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL3_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL4_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL5_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL6_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_COL7_PORT_PIN, GPIO_RESET);
 
 
     /*Set Rows Pins To Be Output*/
-    MGPIO_voidSetPinMode(HLEDMAT_ROW0_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW1_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW2_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW3_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW4_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW5_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW6_PORT_PIN, GPIO_OUTPUT);
-    MGPIO_voidSetPinMode(HLEDMAT_ROW7_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW0_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW1_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW2_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW3_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW4_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW5_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW6_PORT_PIN, GPIO_OUTPUT);
+    GPIO_voidSetPinMode(HLEDMAT_ROW7_PORT_PIN, GPIO_OUTPUT);
     
     /*Set Rows Pins To Be Output PP*/
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW0_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW1_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW2_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW3_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW4_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW5_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW6_PORT_PIN, GPIO_OUTPUT_PP);
-    MGPIO_voidSetPinOutputType(HLEDMAT_ROW7_PORT_PIN, GPIO_OUTPUT_PP);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW0_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW1_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW2_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW3_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW4_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW5_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW6_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
+    GPIO_voidSetPinOutputMode(HLEDMAT_ROW7_PORT_PIN, GPIO_OUTPUT_PUSH_PULL);
 
     /*Set Rows Pin To Be Output Low Speed*/
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW0_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW1_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW2_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW3_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW4_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW5_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW6_PORT_PIN, GPIO_LOW_SPEED);
-    MGPIO_voidSetPinOutputSpeed(HLEDMAT_ROW7_PORT_PIN, GPIO_LOW_SPEED);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW0_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW1_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW2_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW3_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW4_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW5_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW6_PORT_PIN, GPIO_RESET);
+    GPIO_voidSetPinOutputSpeed(HLEDMAT_ROW7_PORT_PIN, GPIO_RESET);
 }
 
 void HLED_voidDisplay(u8 * Copy_pu8Data)
@@ -96,7 +96,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[0]);
         /*Activate Column 0*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL0_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinValue(HLEDMAT_COL0_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -106,7 +106,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[1]);
         /*Activate Column 1*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL1_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL1_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -115,7 +115,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[2]);
         /*Activate Column 2*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL2_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL2_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -124,7 +124,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[3]);
         /*Activate Column 3*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL3_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL3_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
     
@@ -133,7 +133,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[4]);
         /*Activate Column 4*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL4_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL4_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -142,7 +142,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[5]);
         /*Activate Column 5*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL5_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL5_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -151,7 +151,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[6]);
         /*Activate Column 6*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL6_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL6_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
 
@@ -161,7 +161,7 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
         /*Set Rows Value*/
         voidSetRowsValue(Copy_pu8Data[7]);
         /*Activate Column 7*/
-        MGPIO_voidSetPinValue(HLEDMAT_COL7_PORT_PIN, GPIO_PIN_LOW);
+        GPIO_voidSetPinMode(HLEDMAT_COL7_PORT_PIN, GPIO_RESET);
         /*Delay for 2.5msec*/
         MSTK_voidSetBusyWait(2500);
     }
@@ -169,14 +169,14 @@ void HLED_voidDisplay(u8 * Copy_pu8Data)
 
 static void voidDisableAllColumns(void)
 {
-    MGPIO_voidSetPinValue(HLEDMAT_COL0_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL1_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL2_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL3_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL4_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL5_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL6_PORT_PIN, GPIO_PIN_HIGH);
-    MGPIO_voidSetPinValue(HLEDMAT_COL7_PORT_PIN, GPIO_PIN_HIGH);
+    GPIO_voidSetPinMode(HLEDMAT_COL0_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL1_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL2_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL3_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL4_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL5_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL6_PORT_PIN, GPIO_SET);
+    GPIO_voidSetPinMode(HLEDMAT_COL7_PORT_PIN, GPIO_SET);
 }
 
 static void voidSetRowsValue(u8 Copy_u8Value)
@@ -184,19 +184,19 @@ static void voidSetRowsValue(u8 Copy_u8Value)
     u8 Local_u8Bit;
     /*Set Rows Value*/
     Local_u8Bit = GET_BIT(Copy_u8Value, 0);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW0_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW0_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 1);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW1_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW1_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 2);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW2_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW2_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 3);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW3_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW3_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 4);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW4_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW4_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 5);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW5_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW5_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 6);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW6_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW6_PORT_PIN, Local_u8Bit);
     Local_u8Bit = GET_BIT(Copy_u8Value, 7);
-    MGPIO_voidSetPinValue(HLEDMAT_ROW7_PORT_PIN, Local_u8Bit);
+    GPIO_voidSetPinMode(HLEDMAT_ROW7_PORT_PIN, Local_u8Bit);
 }
