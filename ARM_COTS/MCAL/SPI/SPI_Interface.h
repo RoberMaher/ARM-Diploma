21ea5cf9a530d6162d7ac78202522e2983f502d2
robermaher <robermaher@gmail.com>
2024-04-20 10:28:31 +0200

    Adding SPI Driver

---
+++ ARM_COTS/MCAL/SPI/SPI_Interface.h
@@ -0,0 +1,19 @@
+/****************************************************************************************
+* Author:				Rober Maher
+* Creation Data:		06 Apr, 2024
+* Version:				v1.0
+* Compiler:				GNU ARM-GCC
+* Controller:			STM32F401CCU6
+* Layer:				MCAL
+****************************************************************************************/
+/***************************************************************************************
+* Version	  Date				  	Author				  			Description
+* v1.0		  20 Apr, 2024			Rober Maher		  	            Initial Creation
+***************************************************************************************/
+#ifndef SPI_INTERFACE_H
+#define SPI_INTERFACE_H
+
+void MSPI1_voidInit(void);
+u8 MSPI1_u8TrancieveSynchronous(u8 Copy_u8Data);
+
+#endif
